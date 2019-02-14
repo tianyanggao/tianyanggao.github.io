86066f1b899183fa4fb0eddba0ea5bed6357f038
黄奕龙 <yilong.huang@sdce.com.au>
2019-02-14 11:42:38 +1100

    poli test

---
+++ precache-manifest.9fef74cbc94e4c7e1f7adfaaf36c5e36.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "6249697cf2cb101d8a07f0df89efa8c1",
+    "url": "/images/Multi-language-support.png"
+  },
+  {
+    "revision": "4c1634f9b460ef87cd8e",
+    "url": "/vendors.chunk.css"
+  },
+  {
+    "revision": "1429553c127d849a265fe75ecbc42716",
+    "url": "/images/Multi-platform-support.png"
+  },
+  {
+    "revision": "30e0e6f02820db3ee765",
+    "url": "/umi.css"
+  },
+  {
+    "revision": "30e0e6f02820db3ee765",
+    "url": "/umi.js"
+  },
+  {
+    "revision": "4c1634f9b460ef87cd8e",
+    "url": "/vendors.async.js"
+  },
+  {
+    "revision": "a1f63fd7b19a7e8604751bdadcd09b67",
+    "url": "/icons/icon-128x128.png"
+  },
+  {
+    "revision": "b19327e3e83ce06113ae14c0d0497903",
+    "url": "/pwacompat.min.js"
+  },
+  {
+    "revision": "b597d5f8e9497971cacd74ba1c6b9575",
+    "url": "/static/sdce_logo.b597d5f8.svg"
+  },
+  {
+    "revision": "564016af7aef6be82d5d5e0e666ff732",
+    "url": "/images/Multiple-deposit-channels.png"
+  },
+  {
+    "revision": "78c795e77a7dc39aefdf",
+    "url": "/4.async.js"
+  },
+  {
+    "revision": "264f754a4026925bf1f8a269ef97d348",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b16b0f4be159ffb2bea1893e63cceebe",
+    "url": "/images/High-accessibility.png"
+  },
+  {
+    "revision": "194577a7e20bdcc7afbb718f502c134c",
+    "url": "/images/.DS_Store"
+  },
+  {
+    "revision": "472b32f42ac7148a267cfb6a3306d407",
+    "url": "/HomeBanner.jpg"
+  },
+  {
+    "revision": "b4c8be61eb674e83c94476a2ca2459b7",
+    "url": "/images/qrcode.png"
+  },
+  {
+    "revision": "00b5bc29959739f95a5b7e5499bd5a6a",
+    "url": "/images/real-time.png"
+  },
+  {
+    "revision": "27f59708d77efc2fbeca29a0775076f8",
+    "url": "/icons/icon-512x512.png"
+  },
+  {
+    "revision": "735e8f61e5b37303755824e2e88915ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c6d971a3c9b43a6b2ca071b326b0fef",
+    "url": "/images/Security-and-high-performance.png"
+  },
+  {
+    "revision": "00dd84cae2c20d3d7161855d4ac7a2a0",
+    "url": "/icons/icon-192x192.png"
+  },
+  {
+    "revision": "bc7da51422812b84075c49e4ea0c0eba",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "9344842aafd0976fcd638942bf8ffe87",
+    "url": "/images/qrcode-logo.png"
+  },
+  {
+    "revision": "13a438ea96c1ba3f3e24",
+    "url": "/3.chunk.css"
+  },
+  {
+    "revision": "13a438ea96c1ba3f3e24",
+    "url": "/3.async.js"
+  },
+  {
+    "revision": "a5f250eb65f5f5e95504",
+    "url": "/2.chunk.css"
+  },
+  {
+    "revision": "a5f250eb65f5f5e95504",
+    "url": "/2.async.js"
+  },
+  {
+    "revision": "c50e908c8adceb2e5f3ba285b2b71bac",
+    "url": "/.DS_Store"
+  }
+];
